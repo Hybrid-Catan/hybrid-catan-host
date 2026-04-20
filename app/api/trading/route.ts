84b804c515d86769bb083b39a8b136c0206062ea
Pratulw5 <pratul.w5@gmail.com>
2026-04-21 08:21:20 +1000

    feat: completed trading logic between 2 players

--- app/api/trading/route.ts
+++ app/api/trading/route.ts
@@ -1,5 +1,5 @@
 import { GameState, TradeRequest } from "../../../utils/type";
-import { addTradeToGameState, fulfillTrade } from "../../../backend/gamelogic/trading/trading";
+import { addTradeToGameState, cancelTrade, clearTrades, deleteTrade, fulfillTrade } from "../../../backend/gamelogic/trading/trading";
 import { checkReceiverTradeRequest, isTradeActive, checkSenderTradeRequest } from "@/backend/gamelogic/gamerules/tradevalidation";
 
 function addTradeRequest(gameState: GameState, tradeRequest: TradeRequest): GameState | Error {
@@ -21,6 +21,34 @@ function acceptTradeRequest(gameState: GameState, tradeIndex: number): GameState
     return fulfillTrade(gameState, tradeIndex);
 }
 
+function rejectTradeRequest(gameState: GameState, tradeIndex: number): GameState | Error {
+    // if the reciever rejects the trade, we simply mark the trade as inactive and accepted: false
+    return cancelTrade(gameState, tradeIndex);
+}
+
+function cancelTradeRequest(gameState: GameState, tradeIndex: number): GameState | Error {
+    // if the sender cancels the trade, we simply mark the trade as inactive and accepted: false
+    return cancelTrade(gameState, tradeIndex);
+}
+
+function counterTradeRequest(gameState: GameState, tradeIndex: number, newTradeRequest: TradeRequest): GameState | Error {
+    // if the reciever counter the trade, we update the trade with new sending and receiving cards, and mark it as active and accepted: false
+    // we also need to check if the sender has enough resources to offer the counter trade
+    if (!checkSenderTradeRequest(newTradeRequest, gameState)) {
+        return new Error("Sender does not have enough resources.");
+    }
+    let updatedGameState = addTradeToGameState(gameState, newTradeRequest);
+
+    if (!checkReceiverTradeRequest(newTradeRequest, gameState)) {
+        updatedGameState.tradeState.trades[0].canAccept = false;
+    }
+
+    updatedGameState = deleteTrade(updatedGameState, tradeIndex);
+
+    return updatedGameState;
+}
+
 function clearTradeRequest(gameState: GameState, tradeIndex: number): GameState {
-    // clear all trades which are accepted: false after turn
+    // clear all trades which are accepted:false after the turn ends
+    return clearTrades(gameState);
 }
