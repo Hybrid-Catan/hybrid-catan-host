3e2929e5546bb93b531927b9c75d9dd432928a8a
evanng2610 <evanng2610@gmail.com>
2026-04-21 10:43:17 +1000

    fixed a few typos in certain gamelogic files, more to be fixed after my next pull

--- utils/type.ts
+++ utils/type.ts
@@ -45,7 +45,6 @@ export type developmentCards = {
     "VICTORY_POINT": number
 }
 
-
 export type color = "BLUE" | "RED" | "WHITE" | "ORANGE";
 
 /**
@@ -61,7 +60,6 @@ export type Player = {
     "victoryPoints": number,
     "resourceCards": resourceCard,
     "developmentCards": developmentCards,
-
     "pieces": {
         "settlementsPlaced": number,
         "citiesPlaced": number,
@@ -91,20 +89,16 @@ export type GameState = {
     "status": "SETUP" | "IN_PROGRESS" | "FINISHED",
     "players": Player[],// in queue
     "phase": "SETUP_1" | "SETUP_2" | "ROLL" | "TRADE" | "BUILD" | "END",
-
     "dice": {
         "sum": number
     },
-
     "bank": {
         "resourcesCard": resourceCard,
         "developmentCard": number
     },
-
     "tradeState": {
         trades: trade[];
     },
-
     "winner": {
         "playerId": UUID,
     }
